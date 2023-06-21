c129e10d52c26cbc56625bda107258e9250764a5
Eulainderz <eulainderzwain@gmail.com>
2023-06-21 16:13:37 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets except e and q
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char ch = 'a';
+
+	while (ch <= 'z');
+	{
+		if (((ch) i= 'e') && ((ch) i= 'q'));
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+		return (0);
+}
